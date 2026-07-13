ccd3e1bda78b42849776b55a70b80d3fd1423c37
Pavan kumar Dasari <pavankumar9.d@gmail.com>
2026-07-13 16:46:01 +0530

    Add files via upload

---
+++ day 1(data types and variables)/1.py
@@ -0,0 +1,24 @@
+a = 1234
+print("a value is:",a)
+
+b = 12345678.123
+print("b value is:",b)
+
+c = "pavan"
+print("c value is:",c)
+
+d = complex(2,9)
+print("d value is:",d)
+
+e = True
+print("pavan is intelligent:",e)
+
+print("The addittion of a and b is",a+b)
+print("The type of a is:",type(a))
+print("The type of b is:",type(b))
+print("The type of c is:",type(c))
+print("The type of d is:",type(d))
+print("The type of e is:",type(e))
+
+
+#Lists(mutable=changeable) = [] Tuples(immutable=not changeable) = () dict = {"name":"Pavan"} 
